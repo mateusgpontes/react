e2c95fac1f03e68c5a54cc3c409ba490f1f635c0
mateusgpontes <mateus.gpontes@hotmail.com>
2018-06-27 09:47:09 -0300

    Primeiro commit
    Apenas um teste

---
+++ teste/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Eae Mateus aqui</h1>
+          <h2 className="azul">E bem vindo a mais um site!</h2>
+        </header>
+        <section>
+          <p className="azul">You have cat to be kitten me right meow my left donut is missing, as is my right bleghbleghvomit my furball really tie the room together experiences short bursts of poo-phoria after going to the loo. Leave hair everywhere swat turds around the house for more napping, more napping all the napping is exhausting, friends are not food claw at curtains stretch and yawn nibble on tuna ignore human bite human hand.
+          </p>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
